5f89fc0a84282f32ac927a0068b0a29a7ef165ec
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-07-26 22:09:09 +0100

    Use java preferences

--- src/main/java/com/santiagolizardo/madcommander/config/ConfigHandler.java
+++ src/main/java/com/santiagolizardo/madcommander/config/ConfigHandler.java
@@ -19,85 +19,58 @@ package com.santiagolizardo.madcommander.config;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+import java.util.prefs.Preferences;
 import java.util.logging.Logger;
 
 import com.santiagolizardo.madcommander.util.ListsUtils;
 
 public class ConfigHandler {
 
-	private final static Logger logger = Logger.getLogger(ConfigHandler.class.getName());
+	private static final Logger logger = Logger.getLogger(ConfigHandler.class.getName());
 
-	private static final String DIRECTORY_NAME = ".neocommander";
+	private final Preferences prefs = Preferences.userRoot().node("neocommander");
 
 	public void save(ConfigData configData) {
-		var props = new Properties();
-		props.setProperty(
-				"bookmarks",
-				String.join(File.pathSeparator,
-						configData.getBookmarks()));
+		prefs.put("bookmarks", String.join(File.pathSeparator, configData.getBookmarks()));
+		prefs.put("language", configData.getLanguage());
 
-		props.setProperty("language", configData.getLanguage());
+		prefs.putInt("window.width", configData.getWindowSize().width);
+		prefs.putInt("window.height", configData.getWindowSize().height);
 
-		props.setProperty("window.width", String.valueOf(configData.getWindowSize().getWidth()));
-		props.setProperty("window.height", String.valueOf(configData.getWindowSize().getHeight()));
+		prefs.putInt("window.top", configData.getWindowPosition().y);
+		prefs.putInt("window.left", configData.getWindowPosition().x);
 
-		props.setProperty("window.top", String.valueOf(configData.getWindowPosition().getY()));
-		props.setProperty("window.left", String.valueOf(configData.getWindowPosition().getX()));
-
-		var file = new File(System.getProperty("user.home"), DIRECTORY_NAME);
-
-		try (var fos = new FileOutputStream(file)) {
-			props.store(fos, "MadCommander configuration file");
-		} catch (IOException e) {
+		try {
+			prefs.flush();
+		} catch (Exception e) {
 			logger.warning(e.getMessage());
 		}
 	}
 
 	public ConfigData read() {
-		var file = new File(System.getProperty("user.home"), DIRECTORY_NAME);
-
 		var configData = new ConfigData();
 
-		try (var fis = new FileInputStream(file)) {
-
-			var props = new Properties();
-			props.load(fis);
-
-			var bookmarksProp = props.getProperty("bookmarks");
-			if (!bookmarksProp.isEmpty()) {
-				configData.getBookmarks().addAll(ListsUtils.explode(File.pathSeparator,
-						bookmarksProp));
-			}
-
-			var language = props.getProperty("language");
-			if (null != language) {
-				configData.setLanguage(language);
-			}
-
-			var windowWidth = props.getProperty("window.width");
-			var windowHeight = props.getProperty("window.height");
-			if (null != windowWidth && null != windowHeight) {
-				var windowSize = new Dimension(
-						Integer.parseInt(props.getProperty("window.width")),
-						Integer.parseInt(props.getProperty("window.height")));
-				configData.setWindowSize(windowSize);
-			}
-
-			var windowTop = props.getProperty("window.top");
-			var windowLeft = props.getProperty("window.left");
-			if (null != windowTop && null != windowLeft) {
-				var windowPosition = new Point(
-						Integer.parseInt(props.getProperty("window.left")),
-						Integer.parseInt(props.getProperty("window.top")));
-				configData.setWindowPosition(windowPosition);
-			}
-		} catch (IOException e) {
-			logger.warning(e.getMessage());
+		var bookmarksProp = prefs.get("bookmarks", "");
+		if (!bookmarksProp.isEmpty()) {
+			configData.getBookmarks().addAll(ListsUtils.explode(File.pathSeparator, bookmarksProp));
 		}
+
+		var language = prefs.get("language", null);
+		if (null != language) {
+			configData.setLanguage(language);
+		}
+
+		var defaultSize = configData.getWindowSize();
+		var windowSize = new Dimension(
+				prefs.getInt("window.width", defaultSize.width),
+				prefs.getInt("window.height", defaultSize.height));
+		configData.setWindowSize(windowSize);
+
+		var defaultPos = configData.getWindowPosition();
+		var windowPosition = new Point(
+				prefs.getInt("window.left", defaultPos.x),
+				prefs.getInt("window.top", defaultPos.y));
+		configData.setWindowPosition(windowPosition);
 
 		return configData;
 	}
