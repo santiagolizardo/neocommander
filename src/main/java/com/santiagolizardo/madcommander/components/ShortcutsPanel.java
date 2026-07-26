6648a36e419493f26549dc78c70cfe87efb3f96e
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-07-26 22:40:02 +0100

    Update header license

--- src/main/java/com/santiagolizardo/madcommander/components/ShortcutsPanel.java
+++ src/main/java/com/santiagolizardo/madcommander/components/ShortcutsPanel.java
@@ -1,19 +1,22 @@
 /*
-  This file is part of MadCommander, a file manager with two panels.
-
-  MadCommander is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  MadCommander is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with MadCommander.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2026 Santiago Lizardo
+ *
+ * This file is part of NeoCommander.
+ *
+ * NeoCommander is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeoCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.santiagolizardo.madcommander.components;
 
 import com.santiagolizardo.madcommander.MainWindow;
--- src/main/java/com/santiagolizardo/madcommander/util/GlobUtils.java
+++ src/main/java/com/santiagolizardo/madcommander/util/GlobUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Santiago Lizardo
+ *
+ * This file is part of NeoCommander.
+ *
+ * NeoCommander is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeoCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.santiagolizardo.madcommander.util;
 
 import java.util.regex.Pattern;
